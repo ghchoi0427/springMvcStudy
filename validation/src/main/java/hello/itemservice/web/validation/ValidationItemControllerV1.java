a254bcdca34d3e5bd48b8ef08ee29915da007524
ghchoi <ghchoi0427@gmail.com>
2022-08-09 18:18:43 +0900

    feat: 검증 직접 처리

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
@@ -7,8 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.thymeleaf.util.StringUtils;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @Controller
 @RequestMapping("/validation/v1/items")
@@ -38,7 +42,39 @@ public class ValidationItemControllerV1 {
     }
 
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
+
+        //검증 오류 결과 보관
+        Map<String, String> errors = new HashMap<>();
+
+        //검증 로직
+//        if (!StringUtils.hasText(item.getItemName())) {
+//            errors.put("itemName", "상품 이름은 필수입니다.");
+//        }
+        if (Objects.equals(item.getItemName(), "")) {
+            errors.put("itemName", "상품 이름은 필수입니다.");
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            errors.put("price", "가격은 1,000원 ~ 100만원까지 허용합니다.");
+        }
+        if (item.getQuantity() == null || item.getQuantity() > 9999) {
+            errors.put("quantity", "수량은 최대 9,999까지 허용합니다.");
+        }
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값= " + resultPrice);
+            }
+        }
+
+        //검증 실패 시 입력폼으로
+        if (!errors.isEmpty()) {
+            errors.values().forEach(System.out::println);
+            model.addAttribute("errors", errors);
+            return "validation/v1/addForm";
+        }
+
+        //성공 로직
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
